36d7a1cb737766bbfa15f66144fe669b215aa5ef
yingsz <853797147@qq.com>
2019-03-12 16:02:10 +0800

    圆形按钮

---
+++ UGUI/Assets/UICircle/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+
+    public Button button;
+	// Use this for initialization
+	void Start () {
+        button.onClick.AddListener(() =>
+        {
+            Debug.Log("click");
+        });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ UGUI/Assets/UICircle/UICircle.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class UICircle : Image
+{
+    RectTransform m_RectTransform;
+    protected UICircle()
+    {
+        
+    }
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+        vh.Clear();
+    }
+    public override bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+    {
+        if(m_RectTransform == null)
+            m_RectTransform = GetComponent<RectTransform>();
+        Vector2 position;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(m_RectTransform, screenPoint, eventCamera, out position);
+        var w = m_RectTransform.rect.width;
+        var h = m_RectTransform.rect.height;
+        var x = position.x;
+        var y = position.y;
+        var radius = Mathf.Min(w, h) / 2;       
+        var b = x*x +y*y <=radius*radius;
+        Debug.Log(b);
+        return b;
+    }
+
+#if UNITY_EDITOR
+    protected override void Reset()
+    {
+        base.Reset();
+      
+    }
+#endif
+}
+#if UNITY_EDITOR
+[CustomEditor(typeof(UICircle), true)]
+public class UIPolygonInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+    }
+}
+#endif
